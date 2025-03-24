d1bd1aeddb3e1b42d8d20c948e3f0adead230522
Student <francoisadam@hotmail.be>
2025-03-24 21:26:31 +0100

    feat(ingestion): create geography table

--- airflow/dags/data_ingestion_dag.py
+++ airflow/dags/data_ingestion_dag.py
@@ -2,16 +2,20 @@ import os
 from dotenv import load_dotenv 
 
 from helpers.dags_helper import upload_file_to_s3
+from queries.staging import QUERY_DROP_GEOGRAPHY_TABLE_STAGING, QUERY_CREATE_EXTERNAL_GEOGRAPHY_TABLE_STAGING
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.operators.athena import AthenaOperator
 
 # loading variables from .env file
 load_dotenv(dotenv_path="/opt/airflow/.env")
 
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+AWS_ATHENA_STAGING_DATABASE = os.getenv("AWS_ATHENA_STAGING_DATABASE")
+REGION_NAME = os.getenv("REGION_NAME")
 
 PATH_TO_LOCAL_HOME = "/opt/airflow/"
 GEOGRAPHY_DATASET_FILE = "worldcities.csv"
@@ -26,7 +30,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id="data_ingestion_dag",
+    dag_id="geography_data_ingestion_dag",
     schedule_interval="@daily",
     default_args=default_args,
     catchup=False,
@@ -50,4 +54,22 @@ with DAG(
         op_kwargs={"file": f'{GEOGRAPHY_DATASET_FILE}', "table_name": GEOGRAPHY_TABLE_NAME},
     )
     
-    download_geography_dataset_task >> upload_geography_file_to_s3_task 
+    drop_athena_external_table_task = AthenaOperator(
+            task_id='drop_geography_external_table_staging',
+            query=QUERY_DROP_GEOGRAPHY_TABLE_STAGING,
+            database=AWS_ATHENA_STAGING_DATABASE,
+            output_location=f"s3://{AWS_BUCKET_NAME}/{GEOGRAPHY_TABLE_NAME}/athena_output/external/drop/",
+            workgroup='primary',
+            region_name=REGION_NAME
+    )
+    
+    athena_external_table_task = AthenaOperator(
+            task_id='create_geography_external_table_staging',
+            query=QUERY_CREATE_EXTERNAL_GEOGRAPHY_TABLE_STAGING,
+            database=AWS_ATHENA_STAGING_DATABASE,
+            output_location=f"s3://{AWS_BUCKET_NAME}/{GEOGRAPHY_TABLE_NAME}/athena_output/external/create/",
+            workgroup='primary',
+            region_name=REGION_NAME
+    )
+
+    download_geography_dataset_task >> upload_geography_file_to_s3_task >> drop_athena_external_table_task >> athena_external_table_task
